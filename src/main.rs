4f8298f7668f6beb3f0b11039d61f6e715133814
Wabtey <73140258+Wabtey@users.noreply.github.com>
2022-03-02 10:01:21 +0100

    fix rust error
    still get some

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,49 @@
  * to Fullfill our SQL base 'ART MARKET'
 */
 
-/**
- *create One Human base on random name, etc
-*/
-fn createHuman() {
-    println!("Art is dead !");
-    let first_Name = pickARandom("FIRST_NAME");
-    let last_Name = pickARandom("LAST_NAME");
+static FIRST_NAME: &'static [&str] = &["Adrien", "Nelson", "Benoit", "Morgan", "Florian",
+"Thomas", "Maeto", "Clementine", "Stephane"];
 
-    let address = createAddress();
+static LAST_NAME: &'static [&str] = &["Adrien", "Nelson", "Benoit", "Morgan", "Florian",
+"Thomas", "Maeto", "Clementine", "Stephane"];
 
-    //return first_Name, last_Name, address;
+fn createReputation(numberOfCreation: int, baseReput: int) {
+    return rng.gen_range(1..2000);
+}
 
-
+fn createAddress(){
+    return "";
 }
 
 fn createArtist() {
 
+    let mut rng = thread_rng();
+
+    // let FIRST_NAME = (Adrien, Nelson, Benoit, Morgan, Florian,
+    // Thomas, Maeto, Elebane, Stephane)
+
     println!("Art is dead !");
-    let first_Name = pickARandom("FIRST_NAME");
-    let last_Name = pickARandom("LAST_NAME");
+    let first_Name: String = rng.choose(&FIRST_NAME[..0]);
+    let last_Name: String = rng.choose(&LAST_NAME[..0]);
 
     let address = createAddress();
+
+    //createReputation(number of creation, base reput)
+    let reputationartiste = createReputation(10, 0);
 
     let request =
     "INSERT INTO "+
     "<P1_ARTISTE>(nomartiste, webartiste, reputationartiste, adresseartiste)"+
-    "\n VALUES"
+    "\n VALUES ('"+first_Name+" "+last_Name+"',"+
+               "'"+first_Name+"-"+last_Name+".org',"+
+               "'"+reputationartiste+"',"+
+               "'"+address+"',"+
+               ")";
 
+    return request;
+}
+
+fn main() {
+    let request = createArtist();
+    print(request)
 }
