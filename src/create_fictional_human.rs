b0c398a076e65b98cd17358b9fffa9fc58115df7
Wabtey <73140258+Wabtey@users.noreply.github.com>
2022-03-15 15:17:54 +0100

    can create restorer

--- src/create_fictional_human.rs
+++ src/create_fictional_human.rs
@@ -24,6 +24,9 @@ static LIST_NATIONALITY: &'static [&str] = &["nationality unknown", "American", 
 "Serbian", "Singaporean", "Namibian", "Cambodian", "Ghanaian", "Afghan", "Native American", "Lebanese", "Kyrgyzstani", "Vietnamese", "Ugandan", "Cameroonian", "Welsh", "Macedonian", 
 "Puerto Rican", "Catalan", "Filipino", "Sahrawi", "Bangladeshi", "Coptic", "Persian", "Burkinabe", "Beninese", "Sierra Leonean", "Salvadoran"];
 
+static LIST_ARTWORK_TYPE: &'static [&str] = &["Sculture", "Paint", "Ceramics", "Graphic Art", "Illuminated Manuscripts",
+"Jewellery Art", "Metalwork Art", "Mosaic Art", "Photography", "Architecture", "Religious Art", "Rock Art",
+"Stained Glass Art"];
 
 /*
  * cste to keep the unique propriety of id sql
@@ -52,6 +55,8 @@ pub fn create_human() // -> Result<()>
     let reputation_artiste = create_reputation(10, 0);
 
     let mut request: String = "".to_string();
+
+//--COMM-PRISEURS-------------------------------------------------------------------------------
 
     let mut insert_comm =
     "INSERT INTO P1_COMMISSAIRES-PRISEURS (idcommisaire, nomcommisaire, nationalitecommisaire)
@@ -88,16 +93,18 @@ pub fn create_human() // -> Result<()>
         human_n = human_n.replace("display_name", &first_name);
         human_n = human_n.replace("nationality", nationality);
         insert_comm.push_str(&human_n);
-        insert_comm.push_str(","); // have to remove the last one
+        insert_comm.push_str(",");
 
         number_of_creation+=1;
         
     }
 
-    insert_comm.push_str(";END"); //to end the Vec<&str>
+    insert_comm.push_str(";END");
     insert_comm = insert_comm.replace(",;END","; \n \n");
 
     request.push_str(&insert_comm);
+
+//--MECENE--------------------------------------------------------------------------------------
 
     let mut insert_mecene =
     "INSERT INTO P1_MECENE (idmecene, nommecene, reputationmecene, capitalmecene, nationalitemecene)
@@ -135,16 +142,78 @@ pub fn create_human() // -> Result<()>
         human_n = human_n.replace("capital", &capital.to_string());
         human_n = human_n.replace("nationality", nationality);
         insert_mecene.push_str(&human_n);
-        insert_mecene.push_str(","); // have to remove the last one
+        insert_mecene.push_str(",");
 
         number_of_creation+=1;
         
     }
 
-    insert_mecene.push_str(";END"); //to end the Vec<&str>
+    insert_mecene.push_str(";END");
     insert_mecene = insert_mecene.replace(",;END","; \n \n");
 
     request.push_str(&insert_mecene);
+
+//--RESTAURATEUR------------------------------------------------------------------------------------
+
+    let mut insert_restaurateur =
+    "INSERT INTO P1_RESTAURATEUR (idrestaurateur, nomrestaurateur, type, nationaliterestaurateur)
+    \n VALUES ".to_string();
+
+    for i in 0..50 {
+
+        let mut first_name: String =
+            match &LIST_FIRST_NAME.choose(&mut rng) {
+                Some(n) => n.to_string(),
+                None => "Foo".to_string()
+            };
+        let last_name: String =
+            match &LIST_LAST_NAME.choose(&mut rng) {
+                Some(n) => n.to_string(),
+                None => "Bar".to_string()
+            };
+
+        first_name.push_str(" ");
+        first_name.push_str(&last_name);
+
+        let rest_type: String = 
+            match &LIST_ARTWORK_TYPE.choose(&mut rng) {
+                Some(n) => n.to_string(),
+                None => "Undefined".to_string()
+            };
+
+
+        let nationality =
+            match &LIST_NATIONALITY.choose(&mut rng) {
+                Some(n) => n,
+                None => "nationality unknown"
+            };
+        
+        let foobar =
+        "\n (id, 'display_name', type, 'nationality')";
+        let mut human_n = foobar.replace("id", &i.to_string());
+        human_n = human_n.replace("display_name", &first_name);
+        human_n = human_n.replace("type", rest_type)
+        human_n = human_n.replace("nationality", nationality);
+        insert_mecene.push_str(&human_n);
+        insert_mecene.push_str(",");
+
+        number_of_creation+=1;
+        
+    }
+
+    insert_restaurateur.push_str(";END");
+    insert_restaurateur = insert_restaurateur.replace(",;END","; \n \n");
+
+    request.push_str(&insert_restaurateur);
+
+//--CRITIQUE------------------------------------------------------------------------------------
+//--CREANCIER-----------------------------------------------------------------------------------
+//--EXPERT--------------------------------------------------------------------------------------
+//--GALERIE-------------------------------------------------------------------------------------
+//--MARCHE--------------------------------------------------------------------------------------
+//--GALERIE-------------------------------------------------------------------------------------
+
+//--RELATIONS-------------------------------------------------------------------------------------
 
     println!("humans created: {}", number_of_creation);
 
