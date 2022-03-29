18586d65099ffacc5cfd3e2d3817f103d7ebba51
Wabtey <73140258+Wabtey@users.noreply.github.com>
2022-03-30 00:51:38 +0200

    rm error from insert, request, and export a good pdf

--- Part1/dsb_florian_epain_thomas_de_premorel/requetes.sql
+++ Part1/dsb_florian_epain_thomas_de_premorel/requetes.sql
@@ -1,28 +1,26 @@
 -- Projection :
 
-	-- Les noms des oeuvres d'art des Artistes avec un grand A
+	-- Les noms des oeuvres d'art des Artistes avec un grand A (reputation > 1200) avec les artistes en questions.
 
-SELECT titre
-FROM P1_ART Join P1_ARTISTE on nomArtiste
-WHERE reputaionArtiste > 1200
+SELECT P1_ARTISTE.nomartiste, P1_ART.titre
+FROM (P1_CREE JOIN P1_ARTISTE on P1_ARTISTE.idartiste = P1_CREE.idartiste )
+	      JOIN P1_ART on P1_ART.idart = P1_CREE.idart
+WHERE reputationArtiste > 1200
 ;
 
-	-- Les oeuvres d'art dont on ignore le createur
- 
-SELECT titre
-FROM P1_ART
-WHERE nomArtiste IS NULL
-;
+-- Jointure :
 
--- Jointure : Les Mecenes qui achetent aussi de l'art
+	-- Les mecènes et les collectionneurs d'art (créanciers) qui partagent le même nom et prénom.
 
 SELECT *
-FROM P1_MECENE Natural Join P1_CREANCIER on nomCreancier=nomMecene
+FROM P1_MECENE Join P1_CREANCIER on P1_CREANCIER.nomCreancier=P1_MECENE.nomMecene
 ;
 
--- Moyenne sur l'integralite d'un attribut : La moyenne du capital des créanciers
+-- Moyenne sur l'integralite d'un attribut : 
 
-SELECT AVG(capitalCreancier) as capitalCreancier.MOY
+	-- La moyenne du capital supposé des créanciers.
+
+SELECT AVG(capitalCreancier) as moy_capitalcreancier
 FROM P1_CREANCIER
 ;
 
@@ -30,54 +28,60 @@ FROM P1_CREANCIER
 
 	-- Les prix d'entree au Marche d'art les plus bas, actuel (qui ne sont pas expirée)
 	
-SELECT MIN(prixEntryMarche)
+SELECT MIN(prixMarche)
 FROM P1_MARCHE
-WHERE dateMarche > -- dateActuelle
+WHERE dateMarche > CURDATE()
 ;
 			  
 	-- Les Marches d'art dont leur billet d'entree sont en dessous de la moyenne
 	
 SELECT nomMarche
 FROM P1_MARCHE
-WHERE dateMarche > -- dateActuelle
-      prixEntryMarche < (SELECT AVG(prixEntryMarche)
-			  FROM P1_MARCHE
-			  WHERE dateMarche > -- dateActuelle)
+WHERE dateMarche > CURDATE() AND
+      prixMarche < (SELECT AVG(prixMarche)
+					FROM P1_MARCHE)
 ;
 
--- Une Différence : Les artistes qui n'ont rien créé
+-- Une Différence :
+
+	-- Les artistes qui n'ont rien créé
 
 SELECT nomArtiste -- DISTINCT non nécéssaire
 FROM P1_ARTISTE
-WHERE nomArtiste NOT IN (SELECT DISTINCT nomArtiste
-                         FROM P1_ART)
+WHERE idartiste NOT IN (SELECT DISTINCT idartiste
+                         FROM P1_CREE)
 ;
 
--- Une Division : Noms des Creanciers ayant possedé toutes les oeuvres d'art
-	-- peut faire la même chose avec les Musees (Création d'une nouvelle table
-	-- pour se rappeler par quels musées une oeuvre est passée : relation pret works)
+-- Une Division : 
 
-SELECT nomCreancier
+	-- Nom et ID des Creanciers ayant possedé toutes les oeuvres d'art.
+
+SELECT idcreancier, nomCreancier
 FROM P1_CREANCIER
-WHERE (SELECT COUNT(DISTINCT *)
+WHERE (SELECT DISTINCT COUNT(*)
        FROM P1_POSSEDE
-       WHERE P1_POSSEDE.idART = P1_ART.idArt -- P1_POSSEDE.#idArt
-       AND   P1_POSSEDE.idCreancier = P1_CREANCIER.idCreancier)
-      = (SELECT COUNT(DISTINCT *) from P1_ART)
+       WHERE P1_POSSEDE.idcreancier = P1_CREANCIER.idcreancier)
+      =
+	  (SELECT DISTINCT COUNT(*) from P1_ART)
 ;
+
+	-- Nom et ID des Musées ayant disposé de toutes les oeuvres d'art.
+
+SELECT idmusee, nommusee
+FROM P1_MUSEE
+WHERE (SELECT DISTINCT COUNT(*)
+       FROM P1_PRET
+       WHERE P1_PRET.idmusee = P1_MUSEE.idmusee)
+      = (SELECT DISTINCT COUNT(*) from P1_ART)
+;
+
 
 -- Un Group By :
 
-	-- Nombre d'artistes par ville
+	-- Nombre d'artiste par nationalité.
 
-SELECT villeArtiste, COUNT(*)
+SELECT nationaliteArtiste, COUNT(*)
 FROM P1_ARTISTE
-GROUP BY villeArtiste
-;
-
-	-- Donner pour chaque artiste sa cote moyenne (de ses oeuvres)
-	
-SELECT nomArtiste, AVG(cote)
-FROM P1_ARTISTE Join P1_ART
-GROUP BY nomArtiste
+GROUP BY nationaliteArtiste
+ORDER BY COUNT(*) DESC 
 ;
